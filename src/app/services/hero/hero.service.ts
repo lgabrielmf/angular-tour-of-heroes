3b75686a1d1ba57ca172511648881402c8d1c3ea
Luis Gabriel de M Fernandes <lgabrielmf@gmail.com>
2022-08-24 23:05:18 -0300

    Including search component

--- src/app/services/hero/hero.service.ts
+++ src/app/services/hero/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from '../../model/hero';
 import { HEROES } from '../../mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from '../messages/message.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // of = mock observable. Substitute of with the actual http request
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
@@ -23,5 +25,60 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  updateHero(hero: Hero) {
+    this.messageService.add(`HeroService: updated hero id=${hero.id}`);
+    return of(hero);
+  }
+
+  addHero(hero: Hero) {
+    this.messageService.add(`HeroService: added hero id=${hero.id}`);
+    return of(hero);
+  }
+
+  deleteHero(hero: Hero) {
+    this.messageService.add(`HeroService: deleted hero id=${hero.id}`);
+    return of(hero);
+  }
+
+  // TODO - Not working because it's not using a web request
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.getHeroes().pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
